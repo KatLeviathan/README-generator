76c2b71b595c4493009165c6e7fbc39cff5d0380
KatLeviathan <alexisbrown-renshaw@outlook.com>
2024-05-02 21:36:51 -0400

    made the questions to fit the README guidelines

--- Develop/index.js
+++ Develop/index.js
@@ -1,41 +1,58 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
+const inquirer = require('inquirer');
+
 const questions = [
-    inquirer
-  .prompt(
-    {
-      type: 'input',
-      title: 'Project-title',
-      message: 'What is your projects name?',
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
-    },
-    {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
-    },
-    {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  )
+  {
+    type: 'input',
+    name: 'Project-title',
+    message: 'What is your project\'s name?',
+  },
+  {
+    type: 'input',
+    name: 'Motivation',
+    message: 'What was your motivation for this project?',
+  },
+  {
+    type: 'input',
+    name: 'Reason',
+    message: 'Why did you build this project?',
+  },
+  {
+    type: 'input',
+    name: 'Problem-solving',
+    message: 'What problem does it solve?',
+  },
+  {
+    type: 'input',
+    name: 'Learned',
+    message: 'What did you learn?',
+  },
+  {
+    type: 'input',
+    name: 'Installation-guide',
+    message: 'What are the steps required to install your project?'
+  },
+  {
+    type: 'input',
+    name: 'Collaborators?',
+    message: 'List your collaborators, if any.'
+  },
+  {
+    type: 'input',
+    name: 'License',
+    message: 'What license did you use, if any?'
+  },
 ];
+
+inquirer.prompt(questions)
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
