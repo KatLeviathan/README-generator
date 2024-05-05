3850642b182304f37d1b800a8b128bd6bc4a0b15
KatLeviathan <alexisbrown-renshaw@outlook.com>
2024-05-05 12:45:52 -0400

    wrote the function to make the read me file, put the questions in to README format and the function to initialize and call when tested in terminal

--- Develop/index.js
+++ Develop/index.js
@@ -1,68 +1,66 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const questions = [
-  {
-    type: 'input',
-    name: 'Project-title',
-    message: 'What is your project\'s name?',
-  },
-  {
-    type: 'input',
-    name: 'Motivation',
-    message: 'What was your motivation for this project?',
-  },
-  {
-    type: 'input',
-    name: 'Reason',
-    message: 'Why did you build this project?',
-  },
-  {
-    type: 'input',
-    name: 'Problem-solving',
-    message: 'What problem does it solve?',
-  },
-  {
-    type: 'input',
-    name: 'Learned',
-    message: 'What did you learn?',
-  },
-  {
-    type: 'input',
-    name: 'Installation-guide',
-    message: 'What are the steps required to install your project?'
-  },
-  {
-    type: 'input',
-    name: 'Collaborators?',
-    message: 'List your collaborators, if any.'
-  },
-  {
-    type: 'input',
-    name: 'License',
-    message: 'What license did you use, if any?'
-  },
+    { type: 'input', name: 'Project-title', message: 'What is your project\'s name?' },
+    { type: 'input', name: 'Motivation', message: 'What was your motivation for this project?' },
+    { type: 'input', name: 'Reason', message: 'Why did you build this project?' },
+    { type: 'input', name: 'Problem-solving', message: 'What problem does it solve?' },
+    { type: 'input', name: 'Learned', message: 'What did you learn?' },
+    { type: 'input', name: 'Installation-guide', message: 'What are the steps required to install your project?' },
+    { type: 'input', name: 'Collaborators', message: 'List your collaborators, if any.' },
+    { type: 'input', name: 'License', message: 'What license did you use, if any?' }
 ];
 
-inquirer.prompt(questions)
-  .then((answers) => {
-    console.log(answers);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`${fileName} file created successfully`);
+    });
 }
 
-// TODO: Create a function to initialize app
 function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            const readmeContent = `
+# ${answers['Project-title']}
 
+## Motivation
+${answers['Motivation']}
+
+## Reason
+${answers['Reason']}
+
+## Problem-solving
+${answers['Problem-solving']}
+
+## Learned
+${answers['Learned']}
+
+## Installation Guide
+${answers['Installation-guide']}
+
+## Collaborators
+${answers['Collaborators']}
+
+## License
+${answers['License']}
+`;
+
+            writeToFile('README.md', readmeContent);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
 // Function call to initialize app
-init();
+function startApp() {
+    init();
+}
+
+// Call the startApp function to begin the app initialization
+startApp();
